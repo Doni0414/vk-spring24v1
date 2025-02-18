e0deac92db441daad24abebbea7aaa78a0de497a
Doni0414 <nurda.agabek@mail.ru>
2025-02-18 16:27:19 +0500

    Add docker compose

--- api-gateway/src/main/java/com/doni/gateway/config/SecurityBeans.java
+++ api-gateway/src/main/java/com/doni/gateway/config/SecurityBeans.java
@@ -2,8 +2,10 @@ package com.doni.gateway.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.web.server.SecurityWebFilterChain;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.web.server.context.NoOpServerSecurityContextRepository;
 import org.springframework.security.web.server.util.matcher.PathPatternParserServerWebExchangeMatcher;
 
 @Configuration
@@ -13,6 +15,11 @@ public class SecurityBeans {
     public SecurityWebFilterChain securityWebFilterChain(ServerHttpSecurity http) throws Exception {
         return http
                 .securityMatcher(new PathPatternParserServerWebExchangeMatcher("/actuator/**"))
+                .authorizeExchange(configurer -> configurer
+                        .pathMatchers("/actuator/**").hasAuthority("SCOPE_metrics"))
+                .oauth2ResourceServer(configurer -> configurer.jwt(Customizer.withDefaults()))
+                .securityContextRepository(NoOpServerSecurityContextRepository.getInstance())
+                .csrf(ServerHttpSecurity.CsrfSpec::disable)
                 .build();
     }
 }
